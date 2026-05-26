0e8b82d83e02aecabecaa925202fb907b94d0862
Muhammad Bintang Al-Fath <alfathbintangmuhammad@gmail.com>
2026-05-26 15:42:54 +0800

    feat: setup theme system (color, font)

---
+++ Nomi/Core/Theme/AppFonts.swift
@@ -0,0 +1,32 @@
+//
+//  AppFonts.swift
+//  Nomi
+//
+//  Created by Muhammad Bintang Al-Fath on 26/05/26.
+//
+
+import SwiftUI
+
+extension Font {
+
+    // MARK: - Display
+    static let display = Font.custom("Nunito-Black", size: 42)
+
+    // MARK: - Heading
+    static let heading1 = Font.custom("Nunito-Black", size: 28)
+
+    static let heading2 = Font.custom("Nunito-ExtraBold", size: 22)
+
+    static let heading3 = Font.custom("Nunito-ExtraBold", size: 18)
+
+    // MARK: - Body
+    static let bodyLarge = Font.custom("Nunito-SemiBold", size: 16)
+
+    static let bodySmall = Font.custom("Nunito-SemiBold", size: 14)
+
+    // MARK: - Label
+    static let label = Font.custom("Nunito-Bold", size: 12)
+
+    // MARK: - Button
+    static let button = Font.custom("Nunito-ExtraBold", size: 17)
+}
